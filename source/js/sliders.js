1c2dfae9e76254ccc82082bf85afa8f4cec6ef4f
Дамир Бурханов <shanro323@gmail.com>
2024-05-17 18:57:27 +0500

    пятое задание по js-слайдер-убираем коменты и консоль лог

--- source/js/sliders.js
+++ source/js/sliders.js
@@ -1,23 +1,18 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
 
-console.log(document.querySelector('.slide-banner__swiper'))
 const swiper = new Swiper('.slide-banner__swiper', {
     
-    // Optional parameters
-    direction: 'horizontal',//ориентация слайдера
-    loop: true,//зацикленность
+    direction: 'horizontal',
+    loop: true,
   
-    // If we need pagination
-    pagination: {//пагинация
+    pagination: {
       el: '.slide-banner__pagination',
       bulletActiveClass: 'pagination__item--big',
       bulletClass: 'pagination__item',
     },
   
-    // Navigation arrows
-    navigation: {//навигация вперед и назад
+    navigation: {
       nextEl: '.slide-banner__swap--right',
       prevEl: '.slide-banner__swap--left',
     },
   });
-console.log(document.querySelector('.slide-banner__swiper'))
