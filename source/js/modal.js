90469a9210c10be3462bcee04e98f093f51fa496
Дамир Бурханов <shanro323@gmail.com>
2024-06-07 11:35:46 +0500

    не помню какое по счету задание js-мини-правка 2 класса

--- source/js/modal.js
+++ source/js/modal.js
@@ -5,7 +5,7 @@ class Modal {
         this.modal = document.querySelector(`#${idOfElement}`);
         this.modalClose = this.modal.querySelectorAll('.modal__close');
         this.modalContinue = this.modal.querySelectorAll('.modal__continue');
-        this.closeModal = this.closeModal.bind(this);
+        this.closeModal = this.closeModal.bind(this); //Добавил бинд чтоб работала обычная функция закрытия модалки
     }
     
     closeModal () {
@@ -16,6 +16,12 @@ class Modal {
         this.modalClose.forEach(modal => modal.removeEventListener('click', this.closeModal));
         this.modalContinue.forEach(modal => modal.removeEventListener('click', this.closeModal));
     };
+
+    // static closeModal () {
+    //     const modals = document.querySelectorAll('.modal');
+    //     modals.forEach(modal => modal.classList.add('modal--hidden'));
+    //     overlay.classList.toggle('overlay--hidden');
+    // };
 
     openModal () {
         overlay.classList.toggle('overlay--hidden');
